111cb0a5b89f2095eb7ea6519f320b145e839b9b
Niall Thomson <nthomson@pivotal.io>
2019-10-01 07:09:46 -0700

    Create S3 buckets and add information to control plane credhub

---
+++ aws/buckets.tf
@@ -0,0 +1,54 @@
+resource "random_string" "control_plane_bucket_suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket" "control_plane_artifacts" {
+  bucket = "paasify-${var.env_name}-pae-artifacts-${random_string.control_plane_bucket_suffix.result}"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "control_plane_exports" {
+  bucket = "paasify-${var.env_name}-pae-exports-${random_string.control_plane_bucket_suffix.result}"
+  acl    = "private"
+}
+
+resource "aws_iam_user" "control_plane_bucket" {
+  name = "paasify-${var.env_name}-pae-s3"
+}
+
+resource "aws_iam_access_key" "control_plane_bucket" {
+  user = "${aws_iam_user.control_plane_bucket.name}"
+}
+
+resource "aws_iam_user_policy" "control_plane_bucket" {
+  name = "paasify-${var.env_name}-pae-s3"
+  user = "${aws_iam_user.control_plane_bucket.name}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "ListObjectsInBucket",
+            "Effect": "Allow",
+            "Action": ["s3:ListBucket"],
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.control_plane_artifacts.bucket}",
+                "arn:aws:s3:::${aws_s3_bucket.control_plane_exports.bucket}"
+            ]
+        },
+        {
+            "Sid": "AllObjectActions",
+            "Effect": "Allow",
+            "Action": "s3:*Object",
+            "Resource": [
+                "arn:aws:s3:::${aws_s3_bucket.control_plane_artifacts.bucket}/*",
+                "arn:aws:s3:::${aws_s3_bucket.control_plane_exports.bucket}/*"
+            ]
+        }
+    ]
+}
+EOF
+}
